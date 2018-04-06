0eb438171daaec1303c0ea9811c2094985424d70
Quique Espinosa <espinosaquique@hotmail.com>
2018-04-06 13:47:08 -0500

    Inicio del repo

---
+++ reto-uno/src/app/busqueda/busqueda.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+import { Resultado } from '../resultado';//Importa clase
+import { Http, Response} from '@angular/http';//Importa modulo Http y Response
+import 'rxjs/add/operator/map';
+
+
+@Component({
+  selector: 'app-busqueda',
+  templateUrl: './busqueda.component.html',
+  styleUrls: ['./busqueda.component.css']
+})
+
+
+export class BusquedaComponent implements OnInit {
+
+  resultado: Resultado = {
+    displayName: 'Reloj',
+    smallImage: 'http://www.dportenis.mx/wcsstore/ExtendedSitesCatalogAssetStore/images/catalog/thumbnail/4005366-0000V1.jpg',
+    list_Price: '1523.00',
+
+  }
+  
+  //Declara privada la URL de la API
+  private apiURL = 'http://www.liverpool.com.mx/tienda?s=nintendo&d3106047a194921c01969dfdec083925=json';
+  data: any = {};
+  
+  //LLAMA METODOS EN EL CONSTRUCTOR
+  constructor(private http: Http) { 
+    console.log("Inicia llamado duro a la API");
+    this.getResultados();
+    this.getDatos();
+  }
+
+  //METODO PARA OBTENER DATOS
+  getDatos(){
+    return this.http.get(this.apiURL)
+      .map((res: Response) => res.json())  
+  }
+  //METODO PARA OBTENER RESULTADOS
+  getResultados(){
+    this.getDatos().subscribe(data => {
+      console.log(data);
+      this.data = data
+    })
+  }
+
+  ngOnInit() {
+  }
+
+}
---
+++ reto-uno/src/app/resultado.ts
@@ -0,0 +1,5 @@
+export class Resultado{
+    displayName: string;
+    smallImage: string;
+    list_Price: string;
+}
